444d9ecba4357b98a86c7889c5b75e8ba2554aa2
Jesus Perales <jesus.perales@famsa.com>
2017-08-12 17:00:49 -0500

    Cambiando algunas cosas al estilo es6

--- content_scripts/whitenoisefy.js
+++ content_scripts/whitenoisefy.js
@@ -1,8 +1,8 @@
-( function () {
+( () => {
   'use strict';
-  var bufferSize = 4096;
-  var audioContext = new window.AudioContext();
-  var whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1);
+  const BUFFER_SIZE = 4096;
+  let audioContext = new window.AudioContext();
+  let whiteNoise = audioContext.createScriptProcessor(BUFFER_SIZE, 1, 1);
   whiteNoise.onaudioprocess = audioProcess;
 
   function whitenoisefy(request, sender, sendResponse){
@@ -14,10 +14,10 @@
   }
 
   function audioProcess(e) {
-      var output = e.outputBuffer.getChannelData(0);
-      for (var i = 0; i < bufferSize; i++) {
-    output[i] = Math.random() * 2 - 1;
-      }
+    var output = e.outputBuffer.getChannelData(0);
+    for (let i = 0; i < BUFFER_SIZE; i++) {
+      output[i] = Math.random() * 2 - 1;
+    }
   }
 
   browser.runtime.onMessage.addListener(whitenoisefy);
--- popup/whiteNoiseController.js
+++ popup/whiteNoiseController.js
@@ -1,9 +1,12 @@
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("button")) {
-    var action = e.target.textContent;
-    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-    gettingActiveTab.then((tabs) => {
-      browser.runtime.sendMessage( { action: action } );
-    });
-  }
-});
+( () => {
+  'use strict';
+
+  document.addEventListener('click', e => {
+    if ( e.target.classList.contains("button") ) {
+      let action = e.target.textContent;
+      let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+      gettingActiveTab.then( tabs => browser.runtime.sendMessage( { action: action } ) );
+    }
+  });
+
+})();
