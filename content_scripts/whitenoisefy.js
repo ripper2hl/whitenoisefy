bb0f8e25c2451a5372b08e871fd1636b7ba5cfed
Jesus Perales <jesus.perales@famsa.com>
2017-08-12 16:33:20 -0500

    * Funcionando en background

--- content_scripts/whitenoisefy.js
+++ content_scripts/whitenoisefy.js
@@ -1,19 +1,24 @@
-var bufferSize = 4096;
-var audioContext = new window.AudioContext();
-whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1);
-whiteNoise.onaudioprocess = function(e) {
-    var output = e.outputBuffer.getChannelData(0);
-    for (var i = 0; i < bufferSize; i++) {
-	output[i] = Math.random() * 2 - 1;
-    }
-}
+( function () {
+  'use strict';
+  var bufferSize = 4096;
+  var audioContext = new window.AudioContext();
+  var whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1);
+  whiteNoise.onaudioprocess = audioProcess;
 
-function whitenoisefy(request, sender, sendResponse){
-	if(request.action === 'play'){
-		whiteNoise.connect(audioContext.destination);
-	}else{
-		window.whiteNoise.disconnect()
-	}
-}
+  function whitenoisefy(request, sender, sendResponse){
+  	if(request.action === 'play'){
+  		whiteNoise.connect(audioContext.destination);
+  	}else{
+  		whiteNoise.disconnect();
+  	}
+  }
 
-browser.runtime.onMessage.addListener(whitenoisefy);
+  function audioProcess(e) {
+      var output = e.outputBuffer.getChannelData(0);
+      for (var i = 0; i < bufferSize; i++) {
+    output[i] = Math.random() * 2 - 1;
+      }
+  }
+
+  browser.runtime.onMessage.addListener(whitenoisefy);
+})();
--- popup/whiteNoiseController.js
+++ popup/whiteNoiseController.js
@@ -1,13 +1,9 @@
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("button")) {
     var action = e.target.textContent;
-    browser.tabs.executeScript(null, { 
-      file: "/content_scripts/whitenoisefy.js" 
-    });
-
     var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
     gettingActiveTab.then((tabs) => {
-      browser.tabs.sendMessage(tabs[0].id, { action: action });
+      browser.runtime.sendMessage( { action: action } );
     });
   }
 });
